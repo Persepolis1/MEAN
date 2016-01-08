56c17026925a5de9568330b72dea2610a8253c98
Bobak Ansari <electry.ba@gmail.com>
2016-01-08 17:40:28 -0500

    Modified String

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ require('./server/config/routes.js')(app);
 // set up a static file server that points to the "client" directory
 app.use(express.static(path.join(__dirname, './client')));
 app.listen(8000, function() {
-  console.log('cool stuff on: 8000');
+  console.log('Server Online! on: 8000');
 });
